761c677bb397030660cb9104c649687c31fa902b
Nishant <nishantdeshmukh71@gmail.com>
2017-02-07 16:19:26 +0000

    my app staring phase

---
+++ app/controllers/concerns/pages_controller.rb
@@ -0,0 +1,27 @@
+class PagesController < ApplicationController
+    
+   def home
+       
+   end
+   
+   def about
+   
+   end
+   
+   def explore
+   
+   end
+   
+   def partners
+      
+   end
+   
+   def contact_us
+   
+   end
+   
+   
+   
+   
+    
+end
